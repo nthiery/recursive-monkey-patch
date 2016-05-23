8e799137d2cab65dcbd9b9d76783e70eca8f9d62
Nicolas M. Thiéry <nthiery@users.sf.net>
2016-05-23 14:39:00 +0200

    Cleanup setup.py + support for doctests via pytest

--- setup.py
+++ setup.py
@@ -19,83 +19,22 @@ setup(
     version='0.1.0',
     description='Recursive Monkey Patching for Sage',
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/nthiery/recursive-monkey-patch',
-
-    # Author details
     author='Nicolas M. Thiéry',
     author_email='nthiery@users.sf.net',
-
-    # Choose your license
     license='GPLv3',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research'
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: GNU General Public License (GPL)', # TODO Update for GPL v3
         'Programming Language :: Python :: 2',
         #'Programming Language :: Python :: 3',
     ],
-
-    # What does your project relate to?
     keywords='packaging development',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
     py_modules=["recursive_monkey_patch"],
 
-    setup_requires=['nose>=1.0'],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={
-        #'sample': ['package_data.dat'],
-    #},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[], # ('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    #'console_scripts': [
-    #    #    'sample=sample:main',
-    #    #],
-    #},
-
-    #test_suite = 'nose.collector',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 )
