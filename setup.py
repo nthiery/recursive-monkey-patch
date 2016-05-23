17fef985bdcef6741c5a91acbb0f6223675bff27
Nicolas M. Thiéry <nthiery@users.sf.net>
2016-05-23 10:54:29 +0200

    Fixed package name, cleanup useless comments

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='sage-monkey-patch',
+    name='recursive-monkey-patch',
     version='0.1.0',
     description='Recursive Monkey Patching for Sage',
     long_description=long_description,
@@ -40,20 +40,11 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research'
         'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GNU General Public License (GPL)',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 2.6',
-        #'Programming Language :: Python :: 2.7',
         #'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.3',
-        #'Programming Language :: Python :: 3.4',
-        #'Programming Language :: Python :: 3.5',
     ],
 
     # What does your project relate to?
