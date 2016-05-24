b8be8bbc7ad6e5584d361f6b97e6bfb3dc810b88
Nicolas M. Thiéry <nthiery@users.sf.net>
2016-05-25 01:10:52 +0200

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='recursive-monkey-patch',
-    version='0.1.1',
+    version='0.2.0',
     description='Recursive Monkey Patching',
     long_description=long_description,
     url='https://github.com/nthiery/recursive-monkey-patch',
