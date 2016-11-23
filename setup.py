3dce5bfed30da43a1681fc9ea3130264f8c0af5a
Nicolas M. Thiéry <nthiery@users.sf.net>
2016-11-24 00:43:15 +0200

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='recursive-monkey-patch',
-    version='0.2.0',
+    version='0.3.0',
     description='Recursive Monkey Patching',
     long_description=long_description,
     url='https://github.com/nthiery/recursive-monkey-patch',
